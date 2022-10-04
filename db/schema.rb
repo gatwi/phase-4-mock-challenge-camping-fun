c9e33942c2040b0ed35ab7272195bc304ca33968
gatwi <faith.murithi@student.moringaschool.com>
2022-10-04 13:47:32 +0300

    models, migrations and seeding data

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_04_101400) do
+ActiveRecord::Schema.define(version: 2022_10_04_104614) do
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.integer "difficulty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "campers", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "signups", force: :cascade do |t|
+    t.integer "camper_id"
+    t.integer "activity_id"
+    t.integer "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
