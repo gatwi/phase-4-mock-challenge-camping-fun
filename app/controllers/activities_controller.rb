7ab6ede261ccd3885e7a179f6d9247f03989211a
gatwi <faith.murithi@student.moringaschool.com>
2022-10-04 14:13:53 +0300

    mapping routes through controller actions

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,28 @@
-class ActivitiesController < ApplicationController
+class ActivitiesController < ApplicationController 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+    # GET /activities
+    def index
+        render json: Activity.all
+    end 
+
+    # DELETE /activities/:id
+    def destroy
+        activity = find_activity
+        activity.destroy
+        head :no_content
+    end
+
+    private 
+
+    # GET /activities/:id
+    def find_activity
+        Activity.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Activity not found" }, status: :not_found
+    end
+    
 end
